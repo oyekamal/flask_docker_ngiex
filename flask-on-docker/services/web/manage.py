d6dfeb8e58b60a12ccb64674835ab554649430f8
kamalpay <muhammadkamal@paymob.com>
2022-11-16 22:33:17 +0500

    database created add entrypoint.sh added

--- flask-on-docker/services/web/manage.py
+++ flask-on-docker/services/web/manage.py
@@ -1,10 +1,22 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db, User
 
 
 cli = FlaskGroup(app)
 
 
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(email="oyekamalkhan@gmail.com"))
+    db.session.commit()
+    """_summary_
+    """
 if __name__ == "__main__":
     cli()
